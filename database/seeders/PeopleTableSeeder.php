079450f400ab252b19413d4f74359f552ae80be8
KamilSuliga <80487619+KamilSuliga@users.noreply.github.com>
2023-01-16 04:08:51 +0100

    Add files via upload

---
+++ database/seeders/PeopleTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\People;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('people')->delete();
+
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            People::create([
+                'FirstName' => $faker->firstName,
+                'LastName' => $faker->lastName,
+                'Telephone' => $faker->phoneNumber,
+                'Address' => $faker->streetAddress,
+                'city' => $faker->city,
+            ]);
+        }
+    }
+}
